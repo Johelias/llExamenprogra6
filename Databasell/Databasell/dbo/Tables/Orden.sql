646f3ff88a04e9ac425f4833050e31166378030f
Nohelia Sobalvarro <arial8520@gmail.com>
2021-11-20 01:12:36 -0600

    Commit DB

---
+++ Databasell/Databasell/dbo/Tables/Orden.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Orden]
+(
+
+[IdOrden] INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Orden PRIMARY KEY CLUSTERED(IdOrden)
+   , IdProducto int NOT NULL
+   ,CantidadProducto int NOT NULL
+	 CONSTRAINT FK_Orden_Producto FOREIGN KEY(IdProducto) REFERENCES dbo.Producto(IdProducto)
+ 
+
+
+)
